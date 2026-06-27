ac4631c6a99ac80b1a38b68cb41d0f56c2481141
dovudbek7 <dovudbekm99@gmail.com>
2026-06-27 19:42:28 +0500

    correct settings

--- kitob/settings/prod.py
+++ kitob/settings/prod.py
@@ -1,20 +1,20 @@
 from .base import *
 
-# Replace 'yourusername' with your actual PythonAnywhere username
 ALLOWED_HOSTS = [
-    'yourusername.pythonanywhere.com',
+    'dovudbekmm.pythonanywhere.com',
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/yourusername/pdefai-backend/db.sqlite3',
+        'NAME': '/home/dovudbekmm/pdefai-backend/db.sqlite3',
     }
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'https://yourusername.pythonanywhere.com',
+    'https://dovudbekmm.pythonanywhere.com',
+    'https://muslihun.vercel.app',
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/yourusername/pdefai-backend/staticfiles/'
+STATIC_ROOT = '/home/dovudbekmm/pdefai-backend/staticfiles/'
